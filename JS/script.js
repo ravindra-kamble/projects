2de31f5d5a75eceba676ade4f95fd63279b0c747
Ravindra Kamble <hello@bentoc.com>
2022-05-29 11:28:12 +0530

    commit

--- JS/script.js
+++ JS/script.js
@@ -17,15 +17,15 @@ closeBtn.addEventListener("click", function(){
 
 // <!--Start of Tawk.to Script-->
 //<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/627f0f797b967b11798f4a19/1g3058to7';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/627f0f797b967b11798f4a19/1g3058to7';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 //</script>
 // <!--End of Tawk.to Script-->
 
